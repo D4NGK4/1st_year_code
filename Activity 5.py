bd13a8318bd09f2f2a76cdf291e3bd025e4a31e4
D4NGK4 <danjosheps.djt98@gmail.com>
2022-03-28 16:18:26 +0800

    Fixed

--- Activity 5.py
+++ Activity 5.py
@@ -1,29 +1,15 @@
 #Gif, dili diay global ang function??
 print('Submitted by: Dan Joshua M. Tagaan')
-print()
-print()
+print('')
+print('')
 
-num1 = int(input('Enter first number: '))
-num2 = int(input('Enter second number: '))
-choice = print('Select an operator: \n A. Add \n B. Minus \ C. Divide \n D. Multiply ')
+num1 = float(input('Enter first number: '))
+num2 = float(input('Enter second number: '))
 
-if choice == 'A':
-    def addition(add1, add2):
-        return add1 + add2
+def addition(add1, add2):
+    return add1 + add2
 
-    print(addition(num1,num2))
-
-elif choice == 'B':
-    print(min(num1,num2))
-elif choice == 'C':
-    print(divide(num1,num2))
-elif choice == 'D':
-    print(multiply(num1,num2))
-else:
-    print('Invalid Input')
-
-
-def minus (min1, min2):
+def subtract (min1, min2):
     return min1 - min2
 
 def divide(div1,div2):
@@ -31,3 +17,22 @@ def divide(div1,div2):
 
 def multiply(mul1, mul2):
     return mul1 * mul2
+
+choice = input(print('Select an operator: \n A. Add \n B. Minus \n C. Divide \n D. Multiply '))
+
+if choice == 'A':
+    print(str(num1) + ' + ' + str(num2) + ' = ', addition(num1,num2))
+
+elif choice == 'B':
+    print(str(num1) + " - " + str(num2) + " = ", subtract(num1,num2))
+    
+elif choice == 'C':
+    print(str(num1) + ' / ' + str(num2) + ' = ' + divide(num1,num2))
+
+elif choice == 'D':
+    print(str(num1) + ' * ' + str(num2) + ' = ' + multiply(num1,num2))
+else:
+    print('Invalid Input')
+
+
+
