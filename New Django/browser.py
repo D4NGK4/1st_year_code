d2efbda6baced87ddbccb891dcb318730d8b4239
D4NGK4 <danjosheps.djt98@gmail.com>
2022-11-22 19:51:21 +0800

    django new

---
+++ New Django/browser.py
@@ -0,0 +1,14 @@
+import socket
+
+mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysocket.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysocket.send(cmd)
+
+while True:
+    data = mysocket.recv(512)
+    if len(data) < 1 : 
+        break
+    print(data.decode(),end='')
+
+mysocket.close()
